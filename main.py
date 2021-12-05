d92be176be528a0b65bc7eb52153db74f17d3200
Marco Anaya <marco.a.anaya34@gmail.com>
2021-12-04 19:38:03 -0800

    replaced duplicate code with method

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from src.util.day_util import DayUtil
 day_classes = DayUtil.get_day_classes()
 
 if requested := sys.argv[1:]:
-    day_classes = [c for c in day_classes if c.__name__.strip("Day") in requested]
+    day_classes = [c for c in day_classes if c.get_number() in requested]
 
 for Day in day_classes:
     print(Day.__name__)
-    n = Day.__name__.strip("Day")
+    n = Day.get_number()
     print(f"  {n}.1 - {Day.one()}")
     print(f"  {n}.2 - {Day.two()}")
