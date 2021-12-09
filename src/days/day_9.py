127b382e0c737be1e29165c57226bed2f43a0869
Marco Anaya <marco.a.anaya34@gmail.com>
2021-12-09 01:03:04 -0800

    refactoring

--- src/days/day_9.py
+++ src/days/day_9.py
@@ -1,42 +1,43 @@
 import math
+from typing import Any, Callable, Generator
 from .abstract_day import AbstractDay
 from ..util.file_util import FileUtil
 
 class Day9(AbstractDay):
     @staticmethod
-    def input(file_name: str) -> list[list[int]]:
-        return FileUtil.file_to_list(file_name, f=lambda x: list(map(int, x)))
+    def input(file_name: str) -> 'Day9.HeightMap':
+        return Day9.HeightMap(FileUtil.file_to_list(file_name, f=lambda x: list(map(int, x))))
 
     @staticmethod
-    def one(data: list[list[int]]) -> int:
-        count = 0
-        for i in range(len(data)):
-            for j in range(len(data[i])):
-                if all(
-                    data[n][m] > data[i][j]
-                    for n,m in Day9.get_directions(i, j)
-                    if 0 <= n < len(data) and 0 <= m < len(data[i])
-                ):
-                    count += data[i][j] + 1
-        return count
+    def one(data: 'Day9.HeightMap') -> int:
+        return sum(data.flatmap(lambda i, j: data[i][j] + 1, cond=data.is_low_point))
 
     @staticmethod
-    def two(data: list[list[int]]) -> int:
-        seen = set()
-        basins=[]
-        def helper(i: int, j: int) -> int:
-            if not (0 <= i < len(data) and 0 <= j < len(data[i])):
-                return 0
-            if data[i][j] == 9 or (i, j) in seen:
+    def two(data: 'Day9.HeightMap') -> int:
+        seen: set[tuple[int, int]] = set()
+        return math.prod(sorted(data.flatmap(lambda i, j: data.search_basin(i, j, seen)))[-3:])
+
+    class HeightMap(list):
+        def is_low_point(self, i: int, j: int) -> bool:
+            return all(self.neighbormap(i, j, lambda n, m: self[n][m] > self[i][j]))
+
+        def search_basin(self, i: int, j: int, seen: set[tuple[int, int]]) -> int:
+            if self[i][j] == 9 or (i, j) in seen:
                 return 0
             seen.add((i, j))
-            return 1 + sum(helper(n, m) for n, m in Day9.get_directions(i, j))
+            return 1 + sum(self.neighbormap(i, j, lambda n, m: self.search_basin(n, m, seen)))
 
-        for i in range(len(data)):
-            for j in range(len(data[i])):
-                basins.append(helper(i, j))
-        return math.prod(sorted(basins)[-3:])
+        def flatmap(
+            self, f: Callable[[int, int], Any], cond: Callable[[int, int], bool]=lambda i, j: True
+        ) -> Generator[Any, None, None]:
+            yield from (f(i, j) for i in range(len(self)) for j in range(len(self[0])) if cond(i, j))
 
-    @staticmethod
-    def get_directions(i: int, j: int) -> list[tuple[int, int]]:
-        return [(i-1, j), (i, j-1), (i+1, j), (i, j+1)]
+        def neighbormap(self, i: int, j: int, f: Callable[[int, int], Any]) -> Generator[Any, None, None]:
+            yield from (f(n, m) for n,m in self.get_directions(i, j) if self.is_inbounds(n, m))
+
+        def is_inbounds(self, i: int, j: int) -> bool:
+            return 0 <= i < len(self) and 0 <= j < len(self[0])
+
+        @staticmethod
+        def get_directions(i: int, j: int) -> list[tuple[int, int]]:
+            return [(i-1, j), (i, j-1), (i+1, j), (i, j+1)]
